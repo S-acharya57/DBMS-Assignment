2e902f8c35f3f7c1d8d4af01fcc1fb12b6bdc58e
S-acharya57 <acharyasa38@gmail.com>
2023-01-07 20:34:46 +0545

    assignment

--- tutorial-1.sql
+++ tutorial-1.sql
@@ -89,6 +89,7 @@ select * from tbl_Employee;
 -- solution:
 select employee_name from tbl_Works where company_name = 'Hardman';
 
+-- ///////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 -- 2. b. Find the names and cities of residence of all employee who work for First Bank Corporation.
@@ -97,19 +98,29 @@ select employee_name from tbl_Works where company_name = 'Hardman';
 
 select * from tbl_Employee join tbl_Company where tbl_Company.company_name = 'Hardman';
 
+-- ///////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 -- 2. c Find the names, street addresses and cities of residence of all employees who work for First Bank Corporation and earn more than 10,000. 
 
 -- solution: 
-	select employee_name from tbl_Works where company_name = 'Hardman' and salary > 10000;
-    
+	-- subqueries
+    select tbl_Employee.employee_name, tbl_Employee.street, tbl_Employee.city from tbl_Employee, tbl_Works where 
+    tbl_Employee.employee_name = tbl_Works.employee_name and
+    tbl_Works.company_name = 'Hardman' and tbl_Works.salary > 10000;
+ 
+	-- join 
+    select employee_name, street, city from (tbl_Employee join tbl_Works) where 
+    company_name = 'Hardman' and salary>10000;
+ 
+ 
 -- 2. d. Find all employees in the database who live in the same cities as the companies for which they work. 
 
 -- 2. d. Solution 
 
 	-- By using join:
-	select * from tbl_Employee join tbl_Company where tbl_Employee.city = tbl_Company.city;
+	select employee_name, street, tbl_Employee.city from (tbl_Employee join tbl_Company)
+    where tbl_Employee.city = tbl_Company.city;
 
 	-- By using subqueries:
 	select tbl_Employee.employee_name from tbl_Employee, tbl_Works, tbl_Company 
@@ -117,6 +128,185 @@ select * from tbl_Employee join tbl_Company where tbl_Company.company_name = 'Ha
     and tbl_works.company_name = tbl_Company.company_name
     and tbl_Company.city = tbl_Employee.city;
 
+-- ///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+-- 2 e. Find all employees in the database who live in the same cities and on the same streets
+-- as do their managers.
+
+-- 2. e. solution
+
+select tbl_Managers.employee_name, tbl_Managers.manager_name
+from tbl_Managers
+inner join
+tbl_Employee as employees on tbl_Managers.employee_name = employees.employee_name
+inner join 
+tbl_Employee as Managers on tbl_Managers.manager_name = Managers.employee_name
+where
+employees.city = Managers.city
+and employees.street = Managers.street; 
+
+-- ///////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
+-- 2. f. Find all employees in the database who do not work for First Bank Corporation.
+
+-- 2. f. Solution:
+
+	select Tbl_Works.employee_name from tbl_Works where tbl_Works.company_name !='Hardman';
+
+-- ///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    
+-- 2. g. Find all employees in the database who earn more than each employee of Small Bank
+-- Corporation.
+
+-- 2. g. Solution: 
+
+select employee_name, company_name, salary from tbl_Works 
+where salary >
+(select max(tbl_Works.salary) from tbl_Works
+where tbl_Works.company_name = 'Specter');
+
+-- ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+-- 2. h. Assume that the companies may be located in several cities. Find all companies located
+-- in every city in which Small Bank Corporation is located.
+
+-- 2 .h. Solution: 
+
+select company_name from tbl_Company 
+where city =
+(select city from tbl_Company where company_name = 'Hardman');
+
+
+-- /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+-- 2. i. Find all employees who earn more than the average salary of all employees of their company.
+
+-- 2. i. solution:
+
+select tbl_Works.employee_name from 
+(select company_name, avg(salary) as avg_salary
+from tbl_works
+group by company_name) as AVG_SAL
+join tbl_Works on AVG_SAL.company_name = tbl_Works.company_name
+where tbl_Works.salary> AVG_SAL.avg_salary;
+
+-- /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+-- 2. j. Find the company that has the most employees.
+
+-- 2. j. solution:
+
+select company_name, count(employee_name) as count_emp from tbl_Works
+as tbl_Works group by company_name
+order by count_emp desc
+limit 1;
+
+
+-- ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+-- 2. k. Find the company that has the smallest payroll.
+
+
+-- 2. k. solution:
+select company_name from tbl_Works
+order by salary asc
+limit 1;
+
+
+-- /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+-- 2. l. Find those companies whose employees earn a higher salary, on average, than the average salary at First Bank Corporation
+
+-- 2. l.. Solution
+
+select company_name from 
+(select company_name, avg(salary) as avg_sal
+from tbl_Works
+group by company_name) as AVG_SAL
+where AVG_SAL.avg_sal> 
+(select average2 from
+( select company_name, avg(salary) as average2 from tbl_Works
+group by company_name) as all_avg_sal
+where all_avg_sal.company_name = 'Jane');
+
+
+-- //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+-- 3. Consider the relational database of Figure 5. Give an expression in SQL for each of the following queries
+
+-- 3. .a. Modify the database so that Cara now lives in Newtown.
+
+-- 3. a. solution:
+
+update tbl_Employee
+set city = 'Newton' 
+where employee_name = 'Cara';
+
+-- ///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+-- 3. b. Give all employees of First Bank Corporation a 10 percent raise
+-- 3. b. solution:
+
+
+update tbl_Works 
+set salary = 1.1*salary
+where company_name = 'Hardman';
+
+-- ///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+-- 3. c. Give all managers of First Bank Corporation a 10 percent raise.
+
+-- 3. b. solution: 
+
+update tbl_Works
+set salary = 1.1*salary
+where employee_name = any(
+select distinct manager_name from tbl_Managers)
+and company_name = 'Hardman';
+
+-- ///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+-- 3. d.  Give all managers of First Bank Corporation a 10 percent raise unless the salary becomes greater than $100,000;
+-- in such cases, give only a 3 percent raise.
+
+-- 3.d. Solution:
+
+update tbl_Works
+set salary = if(salary<100000,
+	salary*1.1,
+    salary*1.03)
+where employee_name = any(
+select distinct manager_name from tbl_Managers)
+and company_name = 'Hardman';
+
+
+-- ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+-- 3. e. Delete all tuples in the works relation for employees of Small Bank Corporation.
+
+-- 3. e. solution:
+
+
+-- disabling the foreign key check
+set foreign_key_checks = 0;
+
+DELETE tbl_Works , tbl_Employee , tbl_Managers FROM tbl_Works
+        JOIN
+    tbl_Employee ON tbl_Employee.employee_name = tbl_Works.employee_name
+        JOIN
+    tbl_Managers ON tbl_Works.employee_name = tbl_Managers.employee_name 
+WHERE
+    tbl_Works.company_name = 'Hardman';
+
+-- after deletion, enabling the foreign key check
+set foreign_key_checks = 1;
+
+-- ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
